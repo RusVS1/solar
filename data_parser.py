ab38d850e3d431a505b43100e779485a934cac23
Ruslan Smirnyagin <rusvs2004@gmail.com>
2024-12-16 00:14:12 +0800

    upload rad

--- data_parser.py
+++ data_parser.py
@@ -8,6 +8,8 @@ from webdriver_manager.chrome import ChromeDriverManager
 import re
 import pandas as pd
 import os
+from datetime import datetime, timedelta
+import json
 
 chrome_options = Options()
 chrome_options.add_argument("--headless")
@@ -18,14 +20,19 @@ driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), opti
 url = "https://rp5.ru/Погода_в_Иркутске"
 driver.get(url)
 
-wait = WebDriverWait(driver, 10)
-table = wait.until(EC.presence_of_element_located((By.CLASS_NAME, "forecastTable")))
+wait = WebDriverWait(driver, 15)
+button = wait.until(EC.element_to_be_clickable((By.ID, "ftab-0")))
+button.click()
+
+table = wait.until(EC.presence_of_element_located((By.ID, "forecastTable_1_3")))
 rows = table.find_elements(By.TAG_NAME, "tr")
 
 data = []
 
-hours = rows[1].find_elements(By.TAG_NAME, "td") or rows[1].find_elements(By.TAG_NAME, "th")
-hours = [int(hour.text) for hour in hours[1:]]
+hours_raw = rows[1].find_elements(By.TAG_NAME, "td") or rows[1].find_elements(By.TAG_NAME, "th")
+"""for i in hours_raw:
+    print(i.text)"""
+hours = [int(hour.text) for hour in hours_raw[1:-1]]
 
 months_lib = {"января" : 1, "февраля" : 2, "марта" : 3, "апреля" : 4, "мая" : 5, "июня" : 6, 
           "июля" : 7, "августа" : 8, "сентября" : 9, "октября" : 10, "ноября" : 11, "декабря" : 12,}
@@ -40,7 +47,7 @@ for cell in cells:
         month = months_lib[date.group(2)]
         days.append(day)
         months.append(month)
-day_data = []
+"""day_data = []
 month_data = []
 for i in range(7*4 - len(hours)):
     day_data.append(days[0])
@@ -50,7 +57,7 @@ for i in range(1, 7):
         day_data.append(days[i])
         month_data.append(months[i])
 data.append(month_data)
-data.append(day_data)
+data.append(day_data)"""
 data.append(hours)
 
 cells = rows[2].find_elements(By.TAG_NAME, "td") or rows[2].find_elements(By.TAG_NAME, "th")
@@ -72,8 +79,8 @@ for cell in cells:
             clouds.append('')
     except:
         pass
-data.append(cloud_cover[1:])
-data.append(clouds[1:])
+data.append(cloud_cover[1:-1])
+#data.append(clouds[1:-1])
 
 cells = rows[3].find_elements(By.TAG_NAME, "td") or rows[3].find_elements(By.TAG_NAME, "th")
 rainfall = []
@@ -88,12 +95,12 @@ for cell in cells:
             rainfall.append('')
     except:
         pass
-data.append(rainfall[1:])
+data.append(rainfall[1:-1])
 
 for i in range(4, 11):
     raw_data = rows[i].find_elements(By.TAG_NAME, "td") or rows[i].find_elements(By.TAG_NAME, "th")
     raw_data = [raw.text for raw in raw_data]
-    data.append(raw_data[1:])
+    data.append(raw_data[1:-1])
 
 hum_data = rows[11].find_elements(By.TAG_NAME, "td") or rows[11].find_elements(By.TAG_NAME, "th")
 humidity = []
@@ -104,11 +111,49 @@ for cell in hum_data:
         humidity.append(teg_b.group(1))
     else:
         humidity.append(digit)
-data.append(humidity[1:])
+data.append(humidity[1:-1])
 
 df = pd.DataFrame(data)
-df_transposed = df.T
+df = df.T
+index_of_zero = df[df[0] == 0].index[0]
+df = df.iloc[index_of_zero:index_of_zero+24]
+
+tomorrow = datetime.today() + timedelta(days=1)
+
+tomorrow_year = tomorrow.year
+tomorrow_month = tomorrow.month
+tomorrow_day = tomorrow.day
+df['year'] = [tomorrow_year] * 24
+df['month'] = [tomorrow_month] * 24
+df['day'] = [tomorrow_day] * 24
+
+df = df[['year', 'month', 'day'] + [col for col in df.columns if col not in ['year', 'month', 'day']]]
+
+df.columns = ['YEAR','MO','DY','HR','N','W1','F','T','Tt','Po','Ff','FF','f','U']
+columns = ['F','Tt','FF','f']
+df = df.drop(columns, axis=1)
+
+json_dir = os.path.dirname(os.path.abspath(__file__))
+json_file = os.path.join(json_dir, "weather.json") 
+with open(json_file , 'r', encoding='utf-8') as f:
+    replacement_rules = json.load(f)
+
+def replace_weather_condition(text, condition_dict):
+    for condition, value in condition_dict.items():
+        if re.search(condition, text, re.IGNORECASE):
+            return value
+    return text
+
+df['W1'] = df['W1'].apply(lambda x: replace_weather_condition(x, replacement_rules["W1"]))
+df['N'] = df['N'].apply(lambda x: replace_weather_condition(x, replacement_rules["N"]))
+
+rad = "rad.csv"
 script_dir = os.path.dirname(os.path.abspath(__file__))
+rad = os.path.join(script_dir, rad)
+df2 = pd.read_csv(rad, delimiter=';', encoding='utf-8', index_col=False)
+df2['sinα'] = df2['sinα'].str.replace(',', '.').astype(float)
+df2['Ho'] = df2['Ho'].str.replace(',', '.').astype(float)
+df = pd.merge(df, df2[['MO', 'DY', 'HR', 'sinα', 'Ho']], on=['MO', 'DY', 'HR'], how='left')
+
 file = os.path.join(script_dir, "data/" + "data.csv")
-df_transposed.to_csv(file, index=False, header=False, encoding="utf-8")
-    
+df.to_csv(file, index=False, encoding="utf-8")
